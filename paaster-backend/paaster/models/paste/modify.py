57e72ee5684900da1fc9ee3796766fde06681ef3
WardPearce <wardpearce@protonmail.com>
2022-06-05 11:12:22 +1200

    Supports pre 3.10 syntax

--- paaster-backend/paaster/models/paste/modify.py
+++ paaster-backend/paaster/models/paste/modify.py
@@ -6,7 +6,8 @@ Version 3, 19 November 2007
 """
 
 from pydantic import BaseModel
+from typing import Union, Optional
 
 
 class UpdatePaste(BaseModel):
-    delete_after_hours: int | float | None = None
+    delete_after_hours: Optional[Union[int, float]] = None
