3623d6563388afb815e51e576b6ef32893dda8a8
WardPearce <wardpearce@protonmail.com>
2023-02-22 18:30:00 +1300

    Added QR code sharing

---
+++ frontend/src/components/QRCode.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import QrCode from "svelte-qrcode";
+
+  export let pasteId: string;
+  export let b64EncodedRawKey: string;
+  export let isOpen: boolean;
+</script>
+
+{#if isOpen}
+  <div role="dialog" class="modal">
+    <div class="contents">
+      <div class="header">
+        <h2>scan to access paste</h2>
+      </div>
+      <QrCode
+        color="#8478c9"
+        background="#0c0b10"
+        size={500}
+        value={`${window.location.origin}/${pasteId}#${b64EncodedRawKey}`}
+      />
+    </div>
+  </div>
+{/if}
