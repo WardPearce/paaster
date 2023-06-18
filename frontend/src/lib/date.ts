a0359c3212f81eec4d94e228d12257c380c20ac3
WardPearce <wardpearce@protonmail.com>
2023-06-19 11:24:50 +1200

    Minor fix to month pass check

--- frontend/src/lib/date.ts
+++ frontend/src/lib/date.ts
@@ -5,7 +5,7 @@ dayjs.extend(relativeTime);
 
 export function relativeDate(date: string | number | Date | dayjs.Dayjs): string {
   const localDate = dayjs(date);
-  if (localDate.diff(dayjs(), "month") > 0) {
+  if (dayjs().diff(localDate, "month") > 0) {
     return localDate.format("MMMM D, YYYY");
   }
   return localDate.fromNow();
