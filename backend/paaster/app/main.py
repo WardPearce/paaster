ee905670505b1fbc11e378840e1e7ff22fb4647b
WardPearce <wardpearce@protonmail.com>
2023-06-19 10:49:10 +1200

    Added catch for create error & move docs to element

--- backend/paaster/app/main.py
+++ backend/paaster/app/main.py
@@ -2,9 +2,9 @@ from app.controllers import router
 from app.env import SETTINGS
 from app.resources import Sessions
 from motor import motor_asyncio
-from pydantic import AnyUrl, BaseModel
-from pydantic_openapi_schema.v3_1_0 import Contact, Server
-from starlite import CORSConfig, OpenAPIConfig, Starlite
+from pydantic import BaseModel
+from pydantic_openapi_schema.v3_1_0 import Contact, License, Server
+from starlite import CORSConfig, OpenAPIConfig, OpenAPIController, Request, Starlite
 
 
 async def start_motor() -> None:
@@ -14,19 +14,42 @@ async def start_motor() -> None:
     Sessions.mongo = mongo[SETTINGS.mongo.collection]
 
 
+class OpenAPIControllerRouteFix(OpenAPIController):
+    def render_stoplight_elements(self, request: Request) -> str:
+        # Gross hack to overwrite the path for the openapi schema file.
+        # due to reverse proxying.
+        path_copy = str(self.path)
+        self.path = SETTINGS.proxy_urls.backend + self.path
+
+        render = super().render_stoplight_elements(request)
+
+        self.path = path_copy
+        return render
+
+
 app = Starlite(
     route_handlers=[router],
     on_startup=[start_motor],
     openapi_config=OpenAPIConfig(
         **SETTINGS.open_api.dict(),
-        root_schema_site="redoc",
+        root_schema_site="elements",
+        openapi_controller=OpenAPIControllerRouteFix,
+        enabled_endpoints={"openapi.json", "openapi.yaml", "elements"},
         description="OpenAPI specification for paaster.io, you are expected to read our encryption implementation to implement it yourself.",
-        servers=[Server(url=SETTINGS.proxy_urls.backend)],
+        servers=[
+            Server(url=SETTINGS.proxy_urls.backend, description="Production server.")
+        ],
         by_alias=True,
+        terms_of_service="https://paaster.io/terms-of-service",
+        license=License(
+            name="GNU Affero General Public License v3.0",
+            identifier="AGPL-3.0",
+            url="https://github.com/WardPearce/paaster/blob/main/LICENSE",
+        ),
         contact=Contact(
             name="Paaster API team",
             email="wardpearce@pm.me",
-            url=AnyUrl("https://github.com/WardPearce/Paaster", scheme="https"),
+            url="https://github.com/WardPearce/Paaster",
         ),
     ),
     cors_config=CORSConfig(
--- frontend/src/lib/client/PaasterClient.ts
+++ frontend/src/lib/client/PaasterClient.ts
@@ -17,7 +17,7 @@ export class PaasterClient {
 
     constructor(config?: Partial<OpenAPIConfig>, HttpRequest: HttpRequestConstructor = FetchHttpRequest) {
         this.request = new HttpRequest({
-            BASE: config?.BASE ?? 'http://boring.localhost/api',
+            BASE: config?.BASE ?? 'http://localhost/api',
             VERSION: config?.VERSION ?? '2.0.0',
             WITH_CREDENTIALS: config?.WITH_CREDENTIALS ?? false,
             CREDENTIALS: config?.CREDENTIALS ?? 'include',
