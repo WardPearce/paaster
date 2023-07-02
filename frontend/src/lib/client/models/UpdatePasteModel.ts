0bcdade29416215891dcd8644ee4184fa1a4240b
WardPearce <wardpearce@protonmail.com>
2023-07-02 17:09:47 +1200

    Added manually selecting lang

--- frontend/src/lib/client/models/UpdatePasteModel.ts
+++ frontend/src/lib/client/models/UpdatePasteModel.ts
@@ -2,8 +2,11 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { PasteLanguage } from './PasteLanguage';
+
 export type UpdatePasteModel = {
     expires_in_hours?: number;
     access_code?: string;
+    language?: PasteLanguage;
 };
 
