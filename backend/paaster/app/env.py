c04062f96079514de683ce680578ed5f0821054b
WardPearce <wardpearce@pm.me>
2023-10-16 23:07:43 +1300

    Fixed base settings

--- backend/paaster/app/env.py
+++ backend/paaster/app/env.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
-from pydantic import AnyHttpUrl, BaseModel, BaseSettings
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 
 
 class MongoDB(BaseModel):
