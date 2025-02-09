d533fa8277fd60f9c8cf1321e83f08268d5b78bf
WardPearce <wardpearce@pm.me>
2025-02-10 01:33:24 +1300

    Delete max length

--- paaster/src/lib/server/misc.ts
+++
@@ -1,9 +0,0 @@
-import { error } from "@sveltejs/kit";
-
-export function maxLength(value?: any, max: number = 64): any {
-  if (value && value.length > max) {
-    throw error(400, `Value should be less then ${max} characters`);
-  }
-
-  return value;
-}
