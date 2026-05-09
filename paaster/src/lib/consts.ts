e821e6aa14deb96704e1c1eefba6020dc6630e50
WardPearce <wardpearce@pm.me>
2026-05-09 12:34:29 +1200

    Add missingtheme cyberpunk

--- paaster/src/lib/consts.ts
+++ paaster/src/lib/consts.ts
@@ -1,4 +1,33 @@
 export const CHUNK_SIZE = 1 * 1024 * 1024; // 1MB chunk size
 export const MAX_UPLOAD_SIZE = 10 * 1024 * 1024; // 10MB chunk size
 
-export const THEMES = ['light', 'dark', 'black', 'autumn', 'claude', 'coffee', 'corporate', 'cupcake', 'dim', 'dracula', 'ghibli', 'gourmet', 'luxury', 'marshmallow', 'mintlify', 'night', 'pastel', 'perplexity', 'retro', 'shadcn', 'slack', 'soft', 'spotify', 'synthwave', 'valorant', 'vscode', 'winter'];
+export const THEMES = [
+	'light',
+	'dark',
+	'black',
+	'autumn',
+	'claude',
+	'coffee',
+	'corporate',
+	'cupcake',
+	'cyberpunk',
+	'dim',
+	'dracula',
+	'ghibli',
+	'gourmet',
+	'luxury',
+	'marshmallow',
+	'mintlify',
+	'night',
+	'pastel',
+	'perplexity',
+	'retro',
+	'shadcn',
+	'slack',
+	'soft',
+	'spotify',
+	'synthwave',
+	'valorant',
+	'vscode',
+	'winter'
+];
